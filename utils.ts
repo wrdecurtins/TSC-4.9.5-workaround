b3bc79120e8c6356621f7e11dc474befa4f9d8f5
William DeCurtins <william.decurtins@ravenind.com>
2023-05-02 21:24:05 -0500

    modify util type

--- utils.ts
+++ utils.ts
@@ -1,16 +1,15 @@
 import type { Namespace, TFuncKey } from 'i18next';
 import { i18n } from './i18n';
 
-export async function getI18nValue<N extends Namespace>(
+export async function getI18nValue<N extends Namespace & string>(
     namespace: N,
     key: TFuncKey<N>
 ): Promise<string> {
-  const loaded = i18n.hasLoadedNamespace(namespace as string);
+  const loaded = i18n.hasLoadedNamespace(namespace);
 
   if (!loaded) {
-    await i18n.loadNamespaces(namespace as string);
+    await i18n.loadNamespaces(namespace);
   }
-
 
   return i18n.t(key, { ns: namespace });
 }
