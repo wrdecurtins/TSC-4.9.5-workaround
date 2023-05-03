aba94b67792736f4f36a49a0bf7b779d869a845c
William DeCurtins <william.decurtins@ravenind.com>
2023-05-02 21:23:10 -0500

    Fix ts build issues from i18n by modifying type module

--- i18next.d.ts
+++ i18next.d.ts
@@ -1,13 +1,27 @@
 import 'i18next';
-import { defaultNS } from './i18n';
+import { defaultNS, TFu } from './i18n';
 
 import example from './locales/example.json';
 
 declare module 'i18next' {
+  /*
+   * You can redeclare the TFunction to be a simpler type to fix errors like
+   * are on line 14 of utils.ts
+
+   * This will reduce type safety from the t function and related methods
+   * considerably though.
+  */
+  interface TFunction {
+    (key: string, options?: {[key: string]: string}): string
+  }
+
+
   interface CustomTypeOptions {
     defaultNS: typeof defaultNS;
+
+    // Comment out the contents of the resources prop
     resources: {
-      example: typeof example;
+      // example: typeof example;
     };
     // returnNull: false;
   }
