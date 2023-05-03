acf01c284c1e78eadb397185a96fd97b609c20c6
William DeCurtins <william.decurtins@ravenind.com>
2023-05-02 21:24:33 -0500

    setup index page to demo i18next usage

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
 // Import stylesheets
-import './style.css';
+import { i18n } from './i18n';
+import { getI18nValue } from './utils';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement | null = document.getElementById('app');
+
+if(appDiv){
+  const appendInnerHTML = (value: string) => {
+    appDiv.innerHTML = `${appDiv.innerHTML} <h1>i18next Result: ${value}</h1> \n`;
+  }
+
+  appDiv.innerHTML = `<h1>i18next results:</h1>\n`;
+
+  getI18nValue('example', 'assignedWork').then((value) => {
+    appendInnerHTML(value);
+  });
+
+  appDiv.addEventListener('click', () => {
+    const developer = i18n.t('example:developer');
+    const result = i18n.t('example:startedBy', {developer});
+    appendInnerHTML(result);
+  })
+}
